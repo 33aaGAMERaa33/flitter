021ee86631aad725058bc68bc05563745415575d
Luan Gabriel da Silva <137464815+33aaGAMERaa33@users.noreply.github.com>
2025-11-06 22:42:45 -0300

    Update Widget.java

--- src/main/java/com/aagameraa/flitter/material/Widget.java
+++ src/main/java/com/aagameraa/flitter/material/Widget.java
@@ -1,5 +1,7 @@
 package com.aagameraa.flitter.material;
 
+import org.jetbrains.annotations.NotNull;
+
 public abstract class Widget {
-    public abstract Element createElement();
+    public abstract @NotNull Element createElement();
 }
